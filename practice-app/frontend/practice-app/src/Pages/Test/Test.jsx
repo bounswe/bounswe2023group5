a55dcc0a66d03566bfba64440432e8863a3cf0a6
alperen-bircak <bircakalperen@gmail.com>
2023-05-05 18:23:01 +0300

    Main container and scss files added

---
+++ practice-app/frontend/practice-app/src/Pages/Test/Test.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Test() {
+    return ( <> Test </> );
+}
+
+export default Test;
--- practice-app/frontend/practice-app/src/index.js
+++ practice-app/frontend/practice-app/src/index.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import MainContainer from './Containers/MainContainer/MainContainer';
+import Test from './Pages/Test/Test';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainContainer />,
+    children: [
+      {
+        path: "test",
+        element: <Test />
+      }
+    ]
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
