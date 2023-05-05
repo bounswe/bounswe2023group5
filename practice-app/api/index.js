664d1814da3b893457c5e3d6262fbee9ed9860e9
Arda Kabadayı <ardakabadayi@192.168.1.8>
2023-05-05 19:58:11 +0300

    some modules that were forgotten to be imported is added

--- practice-app/api/index.js
+++ practice-app/api/index.js
@@ -3,6 +3,8 @@ dotenv.config();
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
+import errorConst from "./src/shared/errors/const.js";
+import Error from "./src/shared/errors/Error.js";
 
 const app = express();
 
