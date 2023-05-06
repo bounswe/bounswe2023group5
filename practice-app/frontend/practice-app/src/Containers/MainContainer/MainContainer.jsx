4048e4356ad44396bdfdda265440744a5252a86d
Ege Ekşi <egeeksi259@gmail.com>
2023-05-06 14:23:59 +0300

    JSONViewer.js and JSONViewer.css are moved to JSONViewer folder

--- practice-app/frontend/practice-app/src/Containers/MainContainer/MainContainer.jsx
+++ practice-app/frontend/practice-app/src/Containers/MainContainer/MainContainer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import "./MainContainer.scss";
-import JSONViewer from "./JSONViewer"
+import JSONViewer from "../JSONViewer/JSONViewer.js"
 
 function MainContainer() {
     return <div className="main-container">
