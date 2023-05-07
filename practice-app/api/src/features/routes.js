a2a34876f6541f8229898c77dbb8dda3cdbac304
canuzdrn <uzdurancan@gmail.com>
2023-05-07 12:07:38 +0300

    Added new get and post endpoints in addition to their routes to controller, and fixed some comments

--- practice-app/api/src/features/routes.js
+++ practice-app/api/src/features/routes.js
@@ -1,8 +1,12 @@
 import express from "express";
 const router = express.Router();
 import gameByCategoryController from "./gamebycategory/controller/GameByCategoryController.js";
+import cardByNameController from "./hearthstonecard/controller/CardByNameController.js";
 
 router.get("/category", gameByCategoryController.getGamesByEmail);
 router.post("/category", gameByCategoryController.insertGames);
+
+router.get("/card", cardByNameController.getCardsByEmail);
+router.post("/card", cardByNameController.insertCardInfo);
 
 export default router;
