fd7284dd5186942b87297bb5d6d4d21b40e161ef
cisel-zumbul <ciselzumbul09@gmail.com>
2023-05-08 01:22:09 +0300

    Implements insertGameReviews providing a POST endpoint #193

--- practice-app/api/src/features/routes.js
+++ practice-app/api/src/features/routes.js
@@ -1,6 +1,8 @@
 import express from "express";
 const router = express.Router();
 import gameByCategoryController from "./gamebycategory/controller/GameByCategoryController.js";
+import gameReviewController from "./gamereview/controller/GameReviewController.js";
+import GameReview from "./gamereview/schema/gameReviewSchema.js";
 import cardByNameController from "./hearthstonecard/controller/CardByNameController.js";
 
 router.get("/category", gameByCategoryController.getGamesByEmail);
@@ -8,5 +10,7 @@ router.post("/category", gameByCategoryController.insertGames);
 
 router.get("/card", cardByNameController.getCardsByEmail);
 router.post("/card", cardByNameController.insertCardInfo);
+
+router.post("/review", gameReviewController.insertGameReviews);
 
 export default router;
