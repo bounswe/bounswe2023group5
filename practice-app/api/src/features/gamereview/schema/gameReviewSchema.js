cf4bdb068b9934e95a6da669ff27478dfd224f6b
cisel-zumbul <ciselzumbul09@gmail.com>
2023-05-07 17:32:43 +0300

    Added game review schema #191

---
+++ practice-app/api/src/features/gamereview/schema/gameReviewSchema.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const gameByCategorySchema = new Schema(
+  {
+    user_email: {
+        type: String,
+        required: [true, "Email field is required!"],
+    },
+    game_name: {
+        type: String,
+        required: [true, "GameName field is required"]
+    },
+    game_id: {
+      type: Number,
+      required: [true, "GameId field is required!"],
+    },
+    reviews: [{
+        review_text : String, 
+        publish_date: String
+    }],
+  },
+  { timestamps: true }
+);
+
+const GameReview = mongoose.model("gameReview", gameReviewSchema);
+
+export default GameReview;
