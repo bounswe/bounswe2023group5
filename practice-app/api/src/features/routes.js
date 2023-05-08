81cae193f4e7aa968a7788ffdc9e0a7706fbc01c
Deniz <den1z.unal@protonmail.com>
2023-05-08 13:53:41 +0300

    added new endpoints related to third party steamwebapi and its GetOwnedGames endpoint

--- practice-app/api/src/features/routes.js
+++ practice-app/api/src/features/routes.js
@@ -2,11 +2,15 @@ import express from "express";
 const router = express.Router();
 import gameByCategoryController from "./gamebycategory/controller/GameByCategoryController.js";
 import cardByNameController from "./hearthstonecard/controller/CardByNameController.js";
+import gameByUserController from "./gamebyuser/controller/GameByUserController.js";
 
 router.get("/category", gameByCategoryController.getGamesByEmail);
 router.post("/category", gameByCategoryController.insertGames);
 
 router.get("/card", cardByNameController.getCardsByEmail);
 router.post("/card", cardByNameController.insertCardInfo);
+
+router.get("/user", gameByUserController.getGamesByEmail);
+router.post("/user", gameByUserController.insertGames);
 
 export default router;
