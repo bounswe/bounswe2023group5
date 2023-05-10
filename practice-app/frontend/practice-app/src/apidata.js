9bb556b0bb3259be2e3005ef00982ebb65301b5c
Arda Kabadayı <kabadayi_arda@hotmail.com>
2023-05-10 20:15:31 +0300

    GameByCategory page informations are added

--- practice-app/frontend/practice-app/src/apidata.js
+++ practice-app/frontend/practice-app/src/apidata.js
@@ -1,7 +1,8 @@
 import { MockGET, MockPOST } from "./APIFunctions/MockAPI";
+import { CategoryGET, CategoryPOST } from "./APIFunctions/GameByCategoryApi";
 
 const apidata = {
-  "test": {
+  test: {
     name: "Test",
     //jsonUrl: "https://jsonplaceholder.typicode.com/todos",
     postFunction: MockPOST,
@@ -22,19 +23,20 @@ const apidata = {
           options: [
             {
               name: "Newest",
-              value: "newest"
+              value: "newest",
             },
             {
               name: "Most Popular",
-              value: "popularity"
-            }
-          ]
-        }
-      ]
-    }
+              value: "popularity",
+            },
+          ],
+        },
+      ],
+    },
   },
-  "jest": {
+  jest: {
     name: "Jest",
+
     form: {
       buttonText: "Simit",
       inputs: [
@@ -42,12 +44,72 @@ const apidata = {
           type: "text",
           name: "gameName",
           label: "Name of the Game",
+        },
+      ],
+    },
+  },
+  gamebycategory: {
+    name: "Game By Category",
+    postFunction: CategoryPOST,
+    getFunction: CategoryGET,
+    form: {
+      buttonText: "Submit",
+      inputs: [
+        {
+          type: "select",
+          name: "category",
+          label: "Category of the Game",
 
-        }
-
-      ]
-    }
-  }
-}
+          options: [
+            {
+              name: "Action",
+              value: "action",
+            },
+            {
+              name: "Card",
+              value: "card",
+            },
+            {
+              name: "Fighting",
+              value: "fighting",
+            },
+            {
+              name: "Horror",
+              value: "horror",
+            },
+            {
+              name: "Military",
+              value: "military",
+            },
+            {
+              name: "Racing",
+              value: "racing",
+            },
+            {
+              name: "Shooter",
+              value: "shooter",
+            },
+            {
+              name: "Sports",
+              value: "sports",
+            },
+            {
+              name: "Strategy",
+              value: "strategy",
+            },
+            {
+              name: "Super Hero",
+              value: "superhero",
+            },
+            {
+              name: "Survival",
+              value: "survival",
+            },
+          ],
+        },
+      ],
+    },
+  },
+};
 
 export default apidata;
