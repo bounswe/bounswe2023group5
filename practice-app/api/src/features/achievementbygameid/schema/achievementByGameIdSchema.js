888e4935be8c1fef8f47bcd24d88f663e40f8e9f
ali.basaran@boun.edu.tr <ali.basaran@boun.edu.tr>
2023-05-11 18:57:17 +0300

    timestamp true is added to the schema

--- practice-app/api/src/features/achievementbygameid/schema/achievementByGameIdSchema.js
+++ practice-app/api/src/features/achievementbygameid/schema/achievementByGameIdSchema.js
@@ -40,7 +40,9 @@ const achievementByGameIdSchema = new Schema({
             required: [true, "Success Rate field is required!"],
         }
     }
-})
+},
+{ timestamps: true }
+)
 
 const AchievementByGameId = mongoose.model("achievementsByGameId", achievementByGameIdSchema);
 
