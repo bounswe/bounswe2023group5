f32e89890f37ed1fe8f74f2af9fbb8438497de5b
alperen-bircak <bircakalperen@gmail.com>
2023-05-11 12:02:28 +0300

    Add to frontend

--- practice-app/frontend/practice-app/src/apidata.js
+++ practice-app/frontend/practice-app/src/apidata.js
@@ -1,6 +1,42 @@
+import { DealGET, DealPOST } from "./APIFunctions/DealApi";
 import { MockGET, MockPOST } from "./APIFunctions/MockAPI";
 
 const apidata = {
+  "deal": {
+    name: "Deals",
+    postFunction: DealPOST,
+    getFunction: DealGET, 
+    form: {
+      buttonText: "Search Deals",
+      inputs: [
+        {
+          type: "text",
+          name: "title",
+          label: "Game Title"
+        }, 
+        {
+          type: "number",
+          name: "upperPrice",
+          label: "Upper Price",          
+        },
+        {
+          type: "number",
+          name: "dealCount",
+          label: "Deal Count",          
+        },
+        {
+          type: "number",
+          name: "minimumRating",
+          label: "Minimum Steam Rating",          
+        },
+        {
+          type: "bool",
+          name: "onSale",
+          label: "Only Include Games On Sale",          
+        }
+      ]
+    }
+  },
   "test": {
     name: "Test",
     //jsonUrl: "https://jsonplaceholder.typicode.com/todos",
