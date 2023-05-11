e55120d15d3e52923ee084a24b29ac04afc074f0
cisel-zumbul <ciselzumbul09@gmail.com>
2023-05-11 11:58:22 +0300

    Error cases are handled and comments are added

--- practice-app/api/src/features/gamereview/controller/GameReviewController.js
+++ practice-app/api/src/features/gamereview/controller/GameReviewController.js
@@ -12,18 +12,22 @@ class GameReviewController {
 
     async insertGameReviews(req, resp, next) {
 
+        // Reads the game name, user email and sort selection from the body of the request
         const { userEmail, gameNameCriteria, sort } = req.body;
 
+        // If email or game name is empty returns empty field error 
         if (!(userEmail && gameNameCriteria)) {
             next(new EmptyFieldError());
             return;
         }
 
+        // Prepares the headers needed for two external requests
         const headers = {
             'X-RapidAPI-Key': process.env.API_KEY_GAME_REVIEW,
             'X-RapidAPI-Host': 'opencritic-api.p.rapidapi.com'
         }
-
+        
+        // Prepares first external GET request
         const ext1stopts = {
             method: 'GET',
             url: 'https://opencritic-api.p.rapidapi.com/game/search',
@@ -34,11 +38,20 @@ class GameReviewController {
         };
 
         try {
+            // Issues the request to the first endpoint
             const gameResp = await axios.request(ext1stopts);
+
+            // Checks if external api returns success
             if (gameResp.status == 200) {
+
+                // Checks if body of the response is null 
                 if (gameResp.data) {
+
+                    // Gets game id and game name of the game that is on top of the response list
                     const gameId = gameResp.data[0].id;
                     const gameName = gameResp.data[0].name;
+
+                    // Prepares second request using the game id, and request parameter sort 
                     const ext2ndopts = {
                         method: 'GET',
                         url: `https://opencritic-api.p.rapidapi.com/reviews/game/${gameId}`,
@@ -49,8 +62,15 @@ class GameReviewController {
                     };
 
                     const reviewResp = await axios.request(ext2ndopts);
+
+                     // Checks if external api returns success
                     if (reviewResp.status == 200) {
+
+                        // Checks if body of the response is null 
                         if (reviewResp.data) {
+
+                            // Gets top 3 reviews with snippet and gameId matching the requested gameId
+                            // Creates review list containing review objects having snippets as review_text and publish date
                             const reviews = reviewResp.data
                                 .filter(review => review.snippet && review.game.id == gameId)
                                 .slice(0, 3)
@@ -60,67 +80,88 @@ class GameReviewController {
                                         publish_date: r.publishedDate
                                     }
                                 });
-                            const dbObject = {
+
+                            // Creates database objects for the game review table
+                            const gameReviewDbObject = {
                                 user_email: userEmail,
                                 game_name: gameName,
                                 game_id: gameId,
                                 reviews: reviews,
                             }
-                            await GameReview.create(dbObject);
+
+                            // Inserts records to the database table
+                            await GameReview.create(gameReviewDbObject);
+
+                            // Returns response with 'CREATED' status 
                             resp.status(201).json(
-                                successfulResponse("Game reviews are inserted to the database successfully")
+                                successfulResponse(`Game reviews for the game ${gameName} are inserted to the database successfully`)
                             );
                         }
-
+                        else {
+                            next(new NotFoundError(`No review is found for the game: ${gameName}`));
+                        }
+                    }
+                    // If 2nd endpoint doesn't return success returns an External Api Error
+                    else {
+                        next(new ExternalApiError());
                     }
 
                 } else {
-                    next(new Error());
+                    next(new NotFoundError("No game is found with the given name"));
                 }
 
             }
-
-
-        } catch (error) {
+            // If 1st endpoint doesn't return success returns an External Api Error
+            else {
+                next(new ExternalApiError());
+            }
+        } 
+        // Catches any error and sends a generic error response
+        catch (error) {
             console.error(error);
+            next(new Error());
+            return;
         }
 
     }
 
     async getGameReviewsByEmail(req, resp, next) {
 
-         // read the user email from query parameters
+         // Reads the user email from query parameters
          const userEmail = req.query.userEmail;
     
-         // check if userEmail field is given properly via url parameter. If not, returns an error
+         // Checks if userEmail field is given properly via url parameter. If not, returns an Empty Field error
          if (!userEmail) {
            next(new EmptyFieldError());
            return;
          }
 
          try {
+            // Fetches reviews by user email from database and sorts so that the most recent comes up first
             const reviews = await GameReview.find(
                 { user_email: userEmail }
               ).sort({ createdAt: -1 });
             
+            // Checks if reviews list is empty 
             if (typeof reviews !== 'undefined' && reviews.length > 0) {
-                // return the MongoDB data as response to the get request
+                // Returns the MongoDB data as response to the get request
                 resp.status(200).json(reviews);
             }
             else {
-                next(new NotFoundError("There is no game that you've searched for its reviews before"));
+                next(new NotFoundError("There is no game you've searched for its reviews before"));
                 return;
             }
-        } 
-         
+        }
+        // Catches any error and sends a generic error response
         catch (error) {
+            console.error(error);
             next(new Error());
+            return;
         }
 
     }
 
 }
-
 
 const gameReviewController = new GameReviewController();
 
