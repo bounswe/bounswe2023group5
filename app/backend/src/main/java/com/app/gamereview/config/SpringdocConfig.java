1cf2a73a7ec81915d4c4d282edafa70ad6831486
Can <uzdurancan@gmail.com>
2023-10-14 15:53:38 +0300

    Swagger configuration and adding sample endpoint

---
+++ app/backend/src/main/java/com/app/gamereview/config/SpringdocConfig.java
@@ -0,0 +1,17 @@
+package com.app.gamereview.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition
+@Configuration
+public class SpringdocConfig {
+
+    @Bean
+    public OpenAPI baseOpenAPI(){
+        return new OpenAPI().info(new Info().title("Spring Doc").version("1.0.0").description("Spring doc"));
+    }
+}
---
+++ app/backend/src/main/java/com/app/gamereview/model/SampleModel.java
@@ -0,0 +1,8 @@
+package com.app.gamereview.model;
+
+import java.util.UUID;
+
+public class SampleModel {
+    public UUID Id;
+    public String Name;
+}
