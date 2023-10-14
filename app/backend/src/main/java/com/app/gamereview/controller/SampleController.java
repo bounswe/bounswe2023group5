74db46b45070995d09aa0aa54fe81e59d395be30
Can <uzdurancan@gmail.com>
2023-10-14 16:01:09 +0300

    Some comments are added

--- app/backend/src/main/java/com/app/gamereview/controller/SampleController.java
+++ app/backend/src/main/java/com/app/gamereview/controller/SampleController.java
@@ -7,7 +7,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
-
+/*
+    @RestController annotation -> we ensure that Spring knows that this class is a controller class
+    @RequestMapping() -> the string inside the annotation defines the base url of endpoints inside this controller class
+    @GetMapping() -> can provide a string inside (e.g. "/user") which will define the url of that specific endpoint
+    Ex: @GetMapping("/user) -> url of that endpoint will become " /api/sample/user"
+    Also : PostMapping(), PutMapping() etc.
+ */
 @RestController
 @RequestMapping("/api/sample")
 public class SampleController {
