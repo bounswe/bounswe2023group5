70f05f7cffb301ef0a85e21fa74d06dffb9ba6a2
Can <uzdurancan@gmail.com>
2023-10-14 13:45:31 +0300

    Initial commit of backend

---
+++ app/backend/src/main/java/com/app/gamereview/GamereviewApplication.java
@@ -0,0 +1,13 @@
+package com.app.gamereview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamereviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamereviewApplication.class, args);
+	}
+
+}
