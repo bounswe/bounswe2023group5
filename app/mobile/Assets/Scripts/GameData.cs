52689158ca51cec5dcc2aab66e8663baface5969
bilalfomo <bilalatim27@gmail.com>
2023-10-15 13:11:20 +0300

    Create game data according to json file + games json read script

---
+++ app/mobile/Assets/Scripts/GameData.cs
@@ -0,0 +1,27 @@
+﻿public class GameData
+{
+        public GameInfo[] games;
+}
+
+public class GameInfo
+{
+        public string name;
+        public string year;
+        public string info;
+        public CharacterInfo[] characters;
+}
+
+public class CharacterInfo
+{
+        public string name;
+        public string role;
+        public AbilityInfo abilities;
+}
+
+public class AbilityInfo
+{
+        public string ability1;
+        public string ability2;
+        public string ability3;
+        public string ability4;
+}
