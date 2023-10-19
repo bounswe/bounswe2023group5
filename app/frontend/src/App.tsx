16f48caa3bfdda3acacbbfddbd1dcdcee853a460
alperen-bircak <bircakalperen@gmail.com>
2023-10-19 19:51:14 +0300

    Main layout added

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
+import "./globals.scss";
 
 function App() {
   return (
--- app/frontend/src/router.tsx
+++ app/frontend/src/router.tsx
@@ -1,10 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import HelloWorld from "./Pages/HelloWorld/HelloWorld";
+import MainLayout from "./Layout/MainLayout/MainLayout";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <HelloWorld />,
+    element: <MainLayout />,
+    children: [
+      {
+        path: "hello",
+        element: <HelloWorld />,
+      },
+    ],
   },
 ]);
 
