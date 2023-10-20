6a30eb6e5d347174e7814af368e10f98f513c1cc
UnalDeniz <den1z.unal@protonmail.com>
2023-10-21 00:10:39 +0300

    added email sending functionality

--- app/backend/src/main/java/com/app/gamereview/controller/AuthController.java
+++ app/backend/src/main/java/com/app/gamereview/controller/AuthController.java
@@ -4,7 +4,7 @@ package com.app.gamereview.controller;
 import com.app.gamereview.dto.request.RegisterUserRequestDto;
 import com.app.gamereview.model.User;
 import com.app.gamereview.service.AuthService;
-import com.app.gamereview.service.UserService;
+import com.app.gamereview.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +16,10 @@ public class AuthController {
     private final AuthService authService;
 
     @Autowired
+    private EmailService emailService;
+
+
+    @Autowired
     public AuthController(AuthService authService) {
         this.authService = authService;
     }
@@ -24,5 +28,11 @@ public class AuthController {
     public ResponseEntity<User> registerUser(@RequestBody RegisterUserRequestDto registerUserRequestDto){
         User userToCreate = authService.registerUser(registerUserRequestDto);
         return ResponseEntity.ok(userToCreate);
+    }
+
+    @PostMapping("/send-email")
+    public String sendEmail(@RequestParam String to, @RequestParam String subject, @RequestParam String body) {
+        emailService.sendEmail(to, subject, body);
+        return "Email sent successfully!";
     }
 }
---
+++ app/backend/src/main/java/com/app/gamereview/service/EmailService.java
@@ -0,0 +1,28 @@
+package com.app.gamereview.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendEmail(String toEmail,
+                                String subject,
+                                String body
+    ) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("bounswe2023.group5@gmail.com");
+        message.setTo(toEmail);
+        message.setText(body);
+        message.setSubject(subject);
+        mailSender.send(message);
+        System.out.println("Mail Send...");
+
+
+    }
+
+}
