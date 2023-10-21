880ccd855b215c7e4dad5a9f04f9d4c53822ae10
cisel-zumbul <ciselzumbul09@gmail.com>
2023-10-22 01:24:44 +0300

    First draft of the change password page is implemented

---
+++ app/frontend/src/Pages/ChangePassword/ChangePassword.tsx
@@ -0,0 +1,81 @@
+import { Button, Form, Input, Card, message } from "antd";
+import { MailOutlined } from "@ant-design/icons";
+import styles from "./ChangePassword.module.scss";
+
+const ChangePassword = () => {
+  const [form] = Form.useForm();
+
+  const handleChangePassword = () => {
+    const values = form.getFieldsValue();
+    const body = {
+      email: values.email,
+      old_password: values.password,
+      new_password: values.new_password,
+    };
+    message.success("Password is changed successfully!");
+  };
+
+  const onFinishFailed = () => {
+    message.error("Couldn't change the password");
+  };
+
+  return (
+    <Card title="Change Password" className={styles.form}>
+      <Form
+        form={form}
+        layout="vertical"
+        onFinish={handleChangePassword}
+        onFinishFailed={onFinishFailed}
+        size="large"
+      >
+        <Form.Item
+          name="email"
+          rules={[
+            { required: true, message: "" },
+            { type: "email", message: "Please provide a valid email" },
+          ]}
+        >
+          <Input
+            prefix={<MailOutlined className="site-form-item-icon" />}
+            placeholder="Email"
+          />
+        </Form.Item>
+        <Form.Item name="password" rules={[{ required: true, message: "" }]}>
+          <Input placeholder="Password" />
+        </Form.Item>
+
+        <Form.Item name="newpassword" rules={[{ required: true, message: "" }]}>
+          <Input.Password placeholder="Enter a new password" />
+        </Form.Item>
+        <Form.Item
+          name="conf-password"
+          dependencies={["newpassword"]}
+          rules={[
+            {
+              required: true,
+            },
+            ({ getFieldValue }) => ({
+              validator(_, value) {
+                if (!value || getFieldValue("newpassword") === value) {
+                  return Promise.resolve();
+                }
+                return Promise.reject(
+                  new Error("The new password that you entered do not match!")
+                );
+              },
+            }),
+          ]}
+        >
+          <Input.Password placeholder="Confirm the new password" />
+        </Form.Item>
+        <Form.Item style={{ display: "flex", justifyContent: "flex-end" }}>
+          <Button htmlType="submit" type="primary">
+            Change Password
+          </Button>
+        </Form.Item>
+      </Form>
+    </Card>
+  );
+};
+
+export default ChangePassword;
