b18acc198686a16caff1ab0124dfdfe5c02dbf7d
Arda Kabadayı <kabadayi_arda@hotmail.com>
2023-10-22 20:31:48 +0300

    login component is added

---
+++ app/frontend/src/Pages/Login/Login.tsx
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import styles from "./Login.module.css";
+import { useNavigate } from "react-router-dom";
+import {
+  UserOutlined,
+  LockOutlined,
+  EyeTwoTone,
+  EyeInvisibleOutlined,
+} from "@ant-design/icons";
+import { Button, Input } from "antd";
+
+function Login() {
+  const [usernameOrEmail, setUsernameOrEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleLogin = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div className={styles.outerContainer}>
+      <div className={styles.innerContainer}>
+        <h2 className={styles.loginHeader}>Login</h2>
+        <h5 className={styles.loginInfo}>
+          Join the Fun - Login and Conquer the Game World!
+        </h5>
+        <div className={styles.loginForm}>
+          <form onSubmit={(event) => handleLogin(event)}>
+            <Input
+              size="large"
+              placeholder="Username or Email"
+              className={styles.formElem}
+              prefix={
+                <UserOutlined
+                  style={{ fontSize: "1.2rem", marginRight: "0.4rem" }}
+                />
+              }
+              value={usernameOrEmail}
+              onChange={(event) => setUsernameOrEmail(event.target.value)}
+            />
+
+            <Input.Password
+              placeholder="Password"
+              prefix={
+                <LockOutlined
+                  style={{ fontSize: "1.2rem", marginRight: "0.4rem" }}
+                />
+              }
+              iconRender={(visible) =>
+                visible ? (
+                  <EyeTwoTone style={{ fontSize: "1rem" }} />
+                ) : (
+                  <EyeInvisibleOutlined style={{ fontSize: "1rem" }} />
+                )
+              }
+              className={styles.formElem}
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+            />
+
+            <div className={styles.buttonContainer}>
+              <Button
+                type="primary"
+                htmlType="submit"
+                style={{ width: "100%" }}
+              >
+                Login
+              </Button>
+              <Button
+                type="link"
+                onClick={() => navigate("/register")}
+                className={styles.navigationLink}
+              >
+                Don't you have an account? Register
+              </Button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
