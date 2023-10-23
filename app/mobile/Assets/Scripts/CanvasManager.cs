8ea3d7302ffaa45aa065b13166f4f99d10b19b53
said-yolcu <saidyolcu.saidyolcu@gmail.com>
2023-10-23 17:18:40 +0300

    Log out button is added

--- app/mobile/Assets/Scripts/CanvasManager.cs
+++ app/mobile/Assets/Scripts/CanvasManager.cs
@@ -61,6 +61,11 @@ public class CanvasManager : MonoBehaviour
         gamesPage.SetActive(false);
         profilePage.SetActive(false);
     }
+
+    public void HideHomePage()
+    {
+        homePage.SetActive(false);
+    }
     
     
     public void ShowForumPage()
