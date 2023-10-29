828ca333c3a403c67019b83babab7e810e732880
alperen-bircak <bircakalperen@gmail.com>
2023-10-29 21:08:34 +0200

    creating page skeleton

--- app/frontend/src/Pages/GameDetails/GameDetails.tsx
+++ app/frontend/src/Pages/GameDetails/GameDetails.tsx
@@ -1,15 +1,44 @@
+import { StarFilled, StarOutlined } from "@ant-design/icons";
+import styles from "./GameDetails.module.scss";
+import { useState } from "react";
+
 function GameDetails() {
+  const score = 4;
+  const [subPage, setSubPage] = useState<"summary" | "reviews" | "forum">(
+    "summary"
+  );
   return (
-    <div>
-      <div>
-        <div>Pic</div>
-        <div>
-          <h1>Title</h1>
-          <div>stars</div>
+    <div className={styles.container}>
+      <div className={styles.info}>
+        <div className={styles.pictureContainer}>
+          <img src="./placeholders/minecraft.jpg" alt="Alperen Çiseli Seviyo" />
+        </div>
+        <div className={styles.titleContainer}>
+          <h1>Minecraft: Java Edition</h1>
+          <div className={styles.starsContainer}>
+            {[1, 1, 1, 1, 1].map((_, index) =>
+              index <= score - 1 ? (
+                <StarFilled key={index} />
+              ) : (
+                <StarOutlined key={index} />
+              )
+            )}
+          </div>
         </div>
       </div>
-      <div>Menu</div>
-      <div>Info</div>
+      <div className={styles.menu}>
+        {["summary", "reviews", "forum"].map((name) => (
+          <button
+            type="button"
+            className={subPage === name ? styles.active : ""}
+            onClick={() => setSubPage(name as any)}
+            disabled
+          >
+            {name}
+          </button>
+        ))}
+      </div>
+      <div className={styles.subPage}>Info</div>
     </div>
   );
 }
