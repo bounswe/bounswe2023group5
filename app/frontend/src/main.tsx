7c47fed5b61228d8496becc65df6ba002a3b1523
alperen-bircak <bircakalperen@gmail.com>
2023-10-29 16:45:35 +0200

    added global error toast

--- app/frontend/src/main.tsx
+++ app/frontend/src/main.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { QueryCache, QueryClient, QueryClientProvider } from "react-query";
 
 import App from "./App.tsx";
 import { AuthProvider } from "./Components/Hooks/useAuth.tsx";
+import { message } from "antd";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError(error) {
+      const text = (error as Error).message;
+      message.error(text);
+      console.error(error as Error);
+    },
+  }),
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
