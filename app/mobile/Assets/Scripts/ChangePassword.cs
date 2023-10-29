20cbf81c85f2bc6807f56e632d6241496171343b
said-yolcu <saidyolcu.saidyolcu@gmail.com>
2023-10-29 21:16:25 +0300

    Added functionality for change-password mobile to make request to the backend, token is not added yet

--- app/mobile/Assets/Scripts/ChangePassword.cs
+++ app/mobile/Assets/Scripts/ChangePassword.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
@@ -12,7 +15,11 @@ public class ChangePassword : MonoBehaviour
 	public TMP_InputField passwordInputField;
 	public TMP_InputField newPasswordInputField;
 	public TMP_Text errorField;
-	
+	private string url = "http://ec2-16-16-166-22.eu-north-1.compute.amazonaws.com/api/auth/change-password";
+
+
+	private string token =
+		"eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJoYWxpc0BoYWxpcy5jb20iLCJleHAiOjE2OTk0NjUxMzYsImlhdCI6MTY5ODYwMTEzNn0.hMc9CDPEMQJA9StOCE4NLGfycnd5zEQ6FbGGXfxDHkw";	
 	// After the change password message is sent, the user should be navigated to 
 	// another page
 	private CanvasManager canvasManager;
@@ -36,7 +43,7 @@ public class ChangePassword : MonoBehaviour
         
     }
 
-    private void SendPasswordChangeRequest()
+    private async void SendPasswordChangeRequest()
     {
 	    // check the email field
 	    if (emailInputField.text.Contains("@") == false || emailInputField.text.Contains(".") == false)
@@ -54,27 +61,49 @@ public class ChangePassword : MonoBehaviour
 			return;
 	    }
 
+	    // "Password change request" object
 	    PasswordChangeRequest passwordChangeRequest = new PasswordChangeRequest();
-	    passwordChangeRequest.email = emailInputField.text;
-	    passwordChangeRequest.password = passwordInputField.text;
-	    passwordChangeRequest.newPassword = newPasswordInputField.text;
+	    passwordChangeRequest.SetFields(passwordInputField.text, newPasswordInputField.text);
 	    
-	    // Here I will normally put a function to make a password change
-	    // request to the back-end. And depending on the response, print
-	    // a message to the page
-	    
-	    var (isPasswordChanged, message) = ChangePasswordHelper.ChangeUserPassword(passwordChangeRequest);
-
-	    
-	    if (isPasswordChanged)
+	    // Make a request to the server
+	    using (var client = new HttpClient())
 	    {
-		    canvasManager.ShowHomePage();
-		    canvasManager.HideChangePasswordPage();
-	    }else
-		{
-			Debug.Log(message);
-			errorField.text = message;
-		}
-		Debug.Log(isPasswordChanged ? "Password changed" : "Password not changed");
+		    // The backend "change password" endpoint
+		    var endpoint = new Uri(url);
+		    
+		    // The body in json form
+		    var newPostJson = JsonConvert.SerializeObject(passwordChangeRequest);
+		    
+		    // Configure the body
+		    var payload = new StringContent(newPostJson, Encoding.UTF8, "application/json");
+
+		    // Add authorization header
+		    client.DefaultRequestHeaders.Add("Authorization", token);
+		    
+		    // Make the request
+		    var response = await client.PostAsync(endpoint, payload);
+
+		    // Returns true if successful
+		    string responseContent = await response.Content.ReadAsStringAsync();
+		    
+		    
+		    // If returned success
+		    if (responseContent == "true")
+		    {
+			    canvasManager.ShowHomePage();
+			    canvasManager.HideChangePasswordPage();
+		    }
+		    else
+		    {
+			    var message = "cannot change the password";
+			    Debug.Log(message);
+			    errorField.text = message;
+		    }
+		    Debug.Log(responseContent == "true" ? "Password changed" : "Password not changed");
+
+	    }
+	    
+	    
+	    
 	}
 }
--- app/mobile/Assets/Scripts/PasswordChangeRequest.cs
+++ app/mobile/Assets/Scripts/PasswordChangeRequest.cs
@@ -2,11 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PasswordChangeRequest
 {
-    public string email;
 
-    public string password;
+    public string currentPassword;
 
     public string newPassword;
+
+    public void SetFields( string password, string newPassword)
+    {
+        // this.Header.Authorization = token;
+        this.currentPassword = password;
+        this.newPassword = newPassword;
+    }
+
 }
