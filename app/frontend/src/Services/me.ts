2c3544b8c841e2c44f19fac22eb73173dd70d48b
alperen-bircak <bircakalperen@gmail.com>
2023-10-29 18:54:35 +0200

    fixed wrong service on me

--- app/frontend/src/Services/me.ts
+++ app/frontend/src/Services/me.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export async function me() {
-  return await axios.get(import.meta.env.VITE_APP_API_URL + "/me");
+  return await axios.get(import.meta.env.VITE_APP_API_URL + "/auth/me");
 }
