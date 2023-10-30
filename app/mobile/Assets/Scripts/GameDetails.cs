23b3f3fbb2dcbfcf00e5d906d200b13ce96fd9d2
said-yolcu <saidyolcu.saidyolcu@gmail.com>
2023-10-31 00:35:55 +0300

    Summary is printed on the screen

--- app/mobile/Assets/Scripts/GameDetails.cs
+++ app/mobile/Assets/Scripts/GameDetails.cs
@@ -17,14 +17,24 @@ public class GameDetails : MonoBehaviour
     [SerializeField] private TMP_Text headingText;
     [SerializeField] private TMP_Text bottomText;
     private CanvasManager canvasManager;
-
-    public class GameResponse
-    {
-        public string gameName;
-        public string gameDescription;
-        public string gameIcon;
-    }
-
+    
+    private string id;
+    private string createdAt;
+    private bool isDeleted;
+    private string gameName;
+    private string gameDescription;
+    private string gameIcon;
+    private string releaseDate;
+    private List<AddedTag> playerTypes;
+    private List<AddedTag> genre;
+    private AddedTag production;
+    private AddedTag duration;
+    private List<AddedTag> platforms;
+    private List<AddedTag> artStyles;
+    private AddedTag developer;
+    private List<AddedTag> otherTags;
+    private string minSystemReq;
+    
     private void Awake()
     {
         //summaryButton.onClick.AddListener(OnClickedSummaryButton);
@@ -32,23 +42,34 @@ public class GameDetails : MonoBehaviour
         //forumButton.onClick.AddListener(OnClickedForumButton);
         canvasManager = FindObjectOfType(typeof(CanvasManager)) as CanvasManager;
         
-        // Make a request
-        string url = AppVariables.HttpServerUrl + "/game/get-game-list";
-
-        StartCoroutine(Get(url, "sth"));
+        GameDetail("dsgfhjhgds");
     }
 
-    IEnumerator Get(string url, string gameName)
+    public void GameDetail(string gameId)
     {
+        
+        // Make a request
+        string url = AppVariables.HttpServerUrl + "/game/get-game";
+
+        StartCoroutine(Get(url, gameId));
+    }
+
+    IEnumerator Get(string url, string gameId)
+    {
+        url += "?gameId=" + gameId;
+        
+        Debug.Log(url);
+        
         var request = new UnityWebRequest(url, "GET");
         request.downloadHandler = new DownloadHandlerBuffer();
         request.SetRequestHeader("Content-Type", "application/json");
+        
 
         yield return request.SendWebRequest();
 
         var response = request.downloadHandler.text;
 
-        var _gamesData = JsonConvert.DeserializeObject<List<GameResponse>>(response);
+        var _gamesData = JsonConvert.DeserializeObject<GetGameResponse>(response);
         
 
         if (request.responseCode != 200 || _gamesData == null)
@@ -58,7 +79,26 @@ public class GameDetails : MonoBehaviour
         }
         else
         {
-            Debug.Log(_gamesData[0].gameName);
+            // Set all the variables corresponding to fields
+            id = _gamesData.game.id;
+            createdAt = _gamesData.game.createdAt;
+            isDeleted = _gamesData.game.isDeleted;
+            gameName = _gamesData.game.gameName;
+            gameDescription = _gamesData.game.gameDescription;
+            gameIcon = _gamesData.game.gameIcon;
+            releaseDate = _gamesData.game.releaseDate;
+            playerTypes = _gamesData.game.playerTypes;
+            genre = _gamesData.game.genre;
+            production = _gamesData.game.production;
+            duration = _gamesData.game.duration;
+            platforms = _gamesData.game.platforms;
+            artStyles = _gamesData.game.artStyles;
+            developer = _gamesData.game.developer;
+            otherTags = _gamesData.game.otherTags;
+            minSystemReq = _gamesData.game.minSystemReq;
+
+            summaryButton.image.color = Color.blue;
+            bottomText.text = gameDescription;
         }
     }
 }
