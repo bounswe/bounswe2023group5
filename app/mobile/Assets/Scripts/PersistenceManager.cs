8fa61c585e2086a87c8b31287f4f604ba251d4cc
Bilal <bilalatim27@gmail.com>
2023-10-30 16:23:54 +0300

    Add persistence manager

---
+++ app/mobile/Assets/Scripts/PersistenceManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public static class PersistenceManager
+{
+        private static string _userToken = "UserToken";
+        private static string _userName = "UserName";
+        public static string UserToken
+        {
+            get => PlayerPrefs.GetString(_userToken, "");
+            set => PlayerPrefs.SetString(_userToken, value);
+        }
+        
+        public static string UserName
+        {
+            get => PlayerPrefs.GetString(_userName, "");
+            set => PlayerPrefs.SetString(_userName, value);
+        }
+}
