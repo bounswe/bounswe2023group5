33c935eb718dbbc698f372b141b9b702969f340d
alperen-bircak <bircakalperen@gmail.com>
2023-10-30 23:39:36 +0200

    undefined data error fix

--- app/frontend/src/Pages/GameDetails/GameDetails.tsx
+++ app/frontend/src/Pages/GameDetails/GameDetails.tsx
@@ -5,6 +5,7 @@ import Summary from "../../Components/GameDetails/Summary/Summary";
 import { useParams } from "react-router-dom";
 import { getGame } from "../../Services/gamedetail";
 import { useQuery } from "react-query";
+import { PacmanLoader } from "react-spinners";
 
 function formatDate(date: Date) {
   const day = date.getDate();
@@ -34,7 +35,9 @@ function formatDate(date: Date) {
 function GameDetails() {
   const { gameId } = useParams();
 
-  const { data } = useQuery(["game", gameId], () => getGame(gameId!));
+  const { data, isLoading } = useQuery(["game", gameId], () =>
+    getGame(gameId!)
+  );
 
   const score = 4;
   const [subPage, setSubPage] = useState<"summary" | "reviews" | "forum">(
@@ -44,45 +47,53 @@ function GameDetails() {
   const date = new Date();
   return (
     <div className={styles.container}>
-      <div className={styles.info}>
-        <div className={styles.pictureContainer}>
-          <img
-            src={`${import.meta.env.VITE_APP_IMG_URL}${data?.gameIcon}`}
-            alt={data.gameName}
-          />
+      {isLoading ? (
+        <div className={styles.errorContainer}>
+          <PacmanLoader color="#1b4559" size={30} />
         </div>
-        <div className={styles.titleContainer}>
-          <div className={styles.name}>
-            <h1>{data?.gameName}</h1>
-            <span>{formatDate(date)}</span>
+      ) : (
+        <>
+          <div className={styles.info}>
+            <div className={styles.pictureContainer}>
+              <img
+                src={`${import.meta.env.VITE_APP_IMG_URL}${data?.gameIcon}`}
+                alt={data.gameName}
+              />
+            </div>
+            <div className={styles.titleContainer}>
+              <div className={styles.name}>
+                <h1>{data?.gameName}</h1>
+                <span>{formatDate(date)}</span>
+              </div>
+              <div className={styles.starsContainer}>
+                {[1, 1, 1, 1, 1].map((_, index) =>
+                  index <= score - 1 ? (
+                    <StarFilled key={index} />
+                  ) : (
+                    <StarOutlined key={index} />
+                  )
+                )}
+              </div>
+            </div>
           </div>
-          <div className={styles.starsContainer}>
-            {[1, 1, 1, 1, 1].map((_, index) =>
-              index <= score - 1 ? (
-                <StarFilled key={index} />
-              ) : (
-                <StarOutlined key={index} />
-              )
-            )}
+          <div className={styles.menu}>
+            {["summary", "reviews", "forum"].map((name) => (
+              <button
+                key={name}
+                type="button"
+                className={subPage === name ? styles.active : ""}
+                onClick={() => setSubPage(name as any)}
+                disabled
+              >
+                {name}
+              </button>
+            ))}
           </div>
-        </div>
-      </div>
-      <div className={styles.menu}>
-        {["summary", "reviews", "forum"].map((name) => (
-          <button
-            key={name}
-            type="button"
-            className={subPage === name ? styles.active : ""}
-            onClick={() => setSubPage(name as any)}
-            disabled
-          >
-            {name}
-          </button>
-        ))}
-      </div>
-      <div className={styles.subPage}>
-        <Summary game={data} />
-      </div>
+          <div className={styles.subPage}>
+            <Summary game={data} />
+          </div>
+        </>
+      )}
     </div>
   );
 }
