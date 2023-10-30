47e17dd74e42dc6a0e008d6ae64dc1af1583323a
AliBasarann <ali.basaran@boun.edu.tr>
2023-10-31 01:55:59 +0300

    minor register bug is solved

--- app/frontend/src/router.tsx
+++ app/frontend/src/router.tsx
@@ -5,7 +5,6 @@ import ForgotPassword from "./Pages/ForgotPassword/ForgotPassword";
 import ChangePassword from "./Pages/ChangePassword/ChangePassword";
 import Login from "./Pages/Login/Login";
 import Register from "./Pages/Register/Register";
-import axios from "axios";
 
 const router = createBrowserRouter([
   {
