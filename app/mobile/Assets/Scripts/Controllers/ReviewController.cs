0a33c81ac3d2809e5a4f3b0b071dcbb71c8e0fad
Bilal <bilalatim27@gmail.com>
2023-11-13 01:58:15 +0300

    Create review controller script

---
+++ app/mobile/Assets/Scripts/Controllers/ReviewController.cs
@@ -0,0 +1,75 @@
+﻿public class ReviewController
+{
+    
+}
+
+public class ReviewUpdateRequest
+{
+    public string id;
+    public string reviewDescription;
+    public string rating;
+}
+
+public class ReviewCreateRequest
+{
+    public string reviewDescription;
+    public string rating;
+    public string gameId;
+}
+
+public class ReviewCreateResponse
+{
+    public string id;
+    public string createdAt;
+    public bool isDeleted;
+    public string reviewDescription;
+    public string rating;
+    public string gameId;
+    public string reviewedBy;
+    public string overallVote;
+    public string voteCount;
+    public string reportNum;
+}
+
+public class ReviewGetRequest
+{
+    public string id;
+}
+
+public class ReviewGetResponse
+{
+    public string id;
+    public string reviewDescription;
+    public string rating;
+    public string gameId;
+    public string reviewedBy;
+    public string overallVote;
+    public string reportNum;    
+    public string createdAt;
+}
+
+public class ReviewGetAllRequest
+{
+    public string gameId;
+    public string reviewedBy;
+    public bool withDeleted;
+}
+
+public class ReviewGetAllResponse
+{
+    public string id;
+    public string reviewDescription;
+    public string rating;
+    public string gameId;
+    public string reviewedUser;
+    public string overallVote;
+    public string reportNum;    
+    public string createdAt;
+}
+
+public class ReviewDeleteRequest
+{
+    public string id;
+}
+
+
