8e501acd94b5d5c8acf9272aba92f7b0f047007e
Bilal <bilalatim27@gmail.com>
2023-11-13 02:00:54 +0300

    Implematation of game review functionality

---
+++ app/mobile/Assets/GameReview.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+public class GameReview : MonoBehaviour
+{
+    [SerializeField] private Image userImage;
+    [SerializeField] private TMP_Text userNameText;
+    [SerializeField] private TMP_Text reviewDescriptionText;
+    [SerializeField] private TMP_Text reviewRateText;
+
+    private string gameID;
+    
+
+    public void Init(ReviewGetAllResponse reviewInfo)
+    {
+        userNameText.text = reviewInfo.reviewedUser;
+        reviewDescriptionText.text = reviewInfo.reviewDescription;
+        gameID = reviewInfo.gameId;
+        reviewRateText.text = "Rate:" + reviewInfo.rating.ToString();
+    }
+   
+}
