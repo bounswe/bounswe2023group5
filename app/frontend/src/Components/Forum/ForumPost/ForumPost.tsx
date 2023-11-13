43a715be28a438cf8306fc836f3c9778a2232369
alperen-bircak <bircakalperen@gmail.com>
2023-11-13 19:12:53 +0200

    votes added

--- app/frontend/src/Components/Forum/ForumPost/ForumPost.tsx
+++ app/frontend/src/Components/Forum/ForumPost/ForumPost.tsx
@@ -1,10 +1,26 @@
 import { Button } from "antd";
 import { formatDate } from "../../../Library/utils/formatDate";
 import styles from "./ForumPost.module.scss";
+import { ArrowDownOutlined, ArrowUpOutlined } from "@ant-design/icons";
+import { useVote } from "../../Hooks/useVote";
 
-function ForumPost({ post }: { post: any }) {
+function ForumPost({ post, forumId }: { post: any; forumId: string }) {
+  const { upvote, downvote } = useVote({
+    voteType: "POST",
+    typeId: post.id,
+    invalidateKey: ["forum", forumId],
+  });
   return (
     <div className={styles.container}>
+      <div className={styles.vote}>
+        <button type="button" onClick={upvote}>
+          <ArrowUpOutlined />
+        </button>
+        <div>{post.overallVote}</div>
+        <button type="button" onClick={downvote}>
+          <ArrowDownOutlined />
+        </button>
+      </div>
       <div className={styles.title}>{post.title}</div>
       <div className={styles.meta}>
         <span>{post.poster.username}</span>
