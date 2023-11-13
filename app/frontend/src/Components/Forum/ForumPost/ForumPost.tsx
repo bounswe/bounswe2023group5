a79fc0fd937e257b4b9f319e3f008dff029cfd70
Arda Kabadayı <kabadayi_arda@hotmail.com>
2023-11-13 19:25:37 +0300

    allow admin to delete a post

--- app/frontend/src/Components/Forum/ForumPost/ForumPost.tsx
+++ app/frontend/src/Components/Forum/ForumPost/ForumPost.tsx
@@ -1,11 +1,35 @@
 import { Button } from "antd";
 import { formatDate } from "../../../Library/utils/formatDate";
 import styles from "./ForumPost.module.scss";
+import { DeleteFilled } from "@ant-design/icons";
+import { useMutation } from "react-query";
+import { deletePost } from "../../../Services/forum";
+import { useAuth } from "../../Hooks/useAuth";
 
 function ForumPost({ post }: { post: any }) {
+  const { user } = useAuth();
+
+  const isAdmin = user?.role === "ADMIN";
+  const deletePostMutation = useMutation(deletePost, {
+    onSuccess: async () => {
+      alert("You successfully delete the post.");
+    },
+    onError: () => {
+      alert("Something went wrong");
+    },
+  });
+
+  const handleDelete = () => {
+    deletePostMutation.mutate(post.id);
+  };
   return (
     <div className={styles.container}>
-      <div className={styles.title}>{post.title}</div>
+      <div className={styles.titleContainer}>
+        <div className={styles.title}>{post.title}</div>
+        {isAdmin && (
+          <DeleteFilled style={{ color: "red" }} onClick={handleDelete} />
+        )}
+      </div>
       <div className={styles.meta}>
         <span>{post.poster.username}</span>
         <span>{post.createdAt && formatDate(post.createdAt)}</span>
