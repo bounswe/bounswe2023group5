e754f8763f5b0ff13f0c4ec6103863fff8e45048
Deniz Unal <den1z.unal@protonmail.com>
2023-11-16 20:37:16 +0300

    created the file storage service

---
+++ app/backend/src/main/java/com/app/gamereview/service/FileStorageService.java
@@ -0,0 +1,53 @@
+package com.app.gamereview.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class FileStorageService {
+    private final Path baseLocation;
+
+    public FileStorageService() {
+        // Initialize the storage location. This could be configured externally.
+        this.baseLocation = Path.of("images");
+    }
+
+    public String storeFile(MultipartFile file, String folder) throws IOException {
+        Path storageLocation = Path.of(baseLocation + "/" + folder);
+        // Ensure the target directory exists
+        Files.createDirectories(storageLocation);
+
+        // Generate a random and unique filename
+        String originalFilename = Optional.ofNullable(file.getOriginalFilename()).orElse("default");
+        String fileExtension = "";
+
+        // Check if the originalFilename contains a dot
+        int lastDotIndex = originalFilename.lastIndexOf('.');
+        if (lastDotIndex > 0 && lastDotIndex < originalFilename.length() - 1) {
+            fileExtension = originalFilename.substring(lastDotIndex);
+        }
+
+        String newFilename = UUID.randomUUID().toString() + fileExtension;
+
+        // Resolve the target file path
+        Path targetFilePath = storageLocation.resolve(newFilename);
+
+        // Check if the filename is unique, regenerate if necessary
+        while (Files.exists(targetFilePath)) {
+            newFilename = UUID.randomUUID().toString() + fileExtension;
+            targetFilePath = storageLocation.resolve(newFilename);
+        }
+
+        // Copy the file to the target path, overwriting if it already exists
+        Files.copy(file.getInputStream(), targetFilePath, StandardCopyOption.REPLACE_EXISTING);
+
+        return newFilename;
+    }
+}
