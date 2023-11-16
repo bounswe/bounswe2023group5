e2ae4ddd88442e6ce300bafebb7d1b7baf5e4b8d
alperen-bircak <bircakalperen@gmail.com>
2023-11-16 11:03:41 +0200

    sort and search added

--- app/frontend/src/Components/Forum/ForumPost/ForumPost.tsx
+++ app/frontend/src/Components/Forum/ForumPost/ForumPost.tsx
@@ -8,8 +8,8 @@ import { useAuth } from "../../Hooks/useAuth";
 import { ArrowDownOutlined, ArrowUpOutlined } from "@ant-design/icons";
 import { useVote } from "../../Hooks/useVote";
 
-function ForumPost({ post }: { post: any }) {
-  const { user } = useAuth();
+function ForumPost({ post, forumId }: { post: any; forumId: string }) {
+  const { user, isLoggedIn } = useAuth();
 
   const isAdmin = user?.role === "ADMIN";
   const deletePostMutation = useMutation(deletePost, {
@@ -25,7 +25,6 @@ function ForumPost({ post }: { post: any }) {
     deletePostMutation.mutate(post.id);
   };
 
-function ForumPost({ post, forumId }: { post: any; forumId: string }) {
   const { upvote, downvote } = useVote({
     voteType: "POST",
     typeId: post.id,
@@ -34,15 +33,15 @@ function ForumPost({ post, forumId }: { post: any; forumId: string }) {
   return (
     <div className={styles.container}>
       <div className={styles.vote}>
-        <button type="button" onClick={upvote}>
+        <button type="button" onClick={upvote} disabled={!isLoggedIn}>
           <ArrowUpOutlined />
         </button>
         <div>{post.overallVote}</div>
-        <button type="button" onClick={downvote}>
+        <button type="button" onClick={downvote} disabled={!isLoggedIn}>
           <ArrowDownOutlined />
         </button>
       </div>
-       <div className={styles.titleContainer}>
+      <div className={styles.titleContainer}>
         <div className={styles.title}>{post.title}</div>
         {isAdmin && (
           <DeleteFilled style={{ color: "red" }} onClick={handleDelete} />
