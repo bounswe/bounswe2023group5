b337592a6b3eef922229459168b76bb60303b614
alperen-bircak <bircakalperen@gmail.com>
2023-11-20 17:56:50 +0200

    game added to group details

--- app/frontend/src/Pages/Group/Group.tsx
+++ app/frontend/src/Pages/Group/Group.tsx
@@ -4,6 +4,8 @@ import { useQuery } from "react-query";
 import { getGroup } from "../../Services/group";
 import { formatDate } from "../../Library/utils/formatDate";
 import Forum from "../../Components/Forum/Forum";
+import { getGame } from "../../Services/gamedetail";
+import Game from "../../Components/Game/Game";
 
 function Group() {
   const { groupId } = useParams();
@@ -12,18 +14,25 @@ function Group() {
     getGroup(groupId!)
   );
 
+  const { data: game } = useQuery(["game", group.gameId], () =>
+    getGame(group.gameId!)
+  );
+
   return (
     <div className={styles.container}>
       <div className={styles.info}>
-        <div className={styles.name}>
-          <h1>{group.title}</h1>
-          <span>{formatDate(group.createdAt)}</span>
+        <div className={styles.game}>{game && <Game game={game} />}</div>
+        <div className={styles.meta}>
+          <div className={styles.name}>
+            <h1>{group?.title}</h1>
+            <span>{formatDate(group?.createdAt)}</span>
+          </div>
+          <div className={styles.desc}>{group?.description}</div>
         </div>
-        <div className={styles.desc}>{group.description}</div>
       </div>
       <div className={styles.forumTitle}>Forum</div>
       <div className={styles.forum}>
-        <Forum forumId={group.forum} />
+        <Forum forumId={group?.forumId} redirect={`/group/${groupId}`} />
       </div>
     </div>
   );
