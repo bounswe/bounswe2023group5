071540335ed25960276dfeeaf9c55056e07675ef
Bilal <bilalatim27@gmail.com>
2023-11-21 15:59:30 +0300

    Implement Ban Group User Functionality

---
+++ app/mobile/Assets/Scripts/GroupRequests/BanGroupUser.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class BanGroupUser : MonoBehaviour
+{
+    private string groupId;
+    private string userId;
+
+    private void Start()
+    {
+        Init(new[] {"groupId","userId"},new[] {"",""});
+
+    }
+
+    public void Init(string[] pars, string[] vals)
+    {
+        groupId  = ListToQueryParameters.GetValueOfParam(
+            pars, vals, "groupId" );
+        userId  = ListToQueryParameters.GetValueOfParam(
+            pars, vals, "userId" );
+        
+        if (groupId == "" || userId == "")
+        {
+            Debug.Log("groupId and userId must be specified");
+        }
+
+        string url = AppVariables.HttpServerUrl + "/group/ban-user" + 
+                     ListToQueryParameters.ListToQueryParams(pars, vals);
+        StartCoroutine(PUT(url));
+    }
+    
+    IEnumerator PUT(string url)
+    {
+        var request = new UnityWebRequest(url, "PUT");
+        request.downloadHandler = new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+        request.SetRequestHeader("Authorization", PersistenceManager.UserToken);
+        yield return request.SendWebRequest();
+        string response = "";
+        if (request.responseCode == 200)
+        {
+            response = request.downloadHandler.text;
+            Debug.Log("Success to ban group user: " + response);
+        }
+        else
+        {
+            Debug.Log("Error to ban group user: " + response);
+        }
+        request.downloadHandler.Dispose();
+    }
+    
+}
+
