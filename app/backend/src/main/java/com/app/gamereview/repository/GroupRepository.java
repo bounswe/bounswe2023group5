72546d88521b698c4701511636d41cb1043c1a4f
Halis Bal <halis@kunduz.com>
2023-11-26 16:13:41 +0300

    implemented profile page endpoints

--- app/backend/src/main/java/com/app/gamereview/repository/GroupRepository.java
+++ app/backend/src/main/java/com/app/gamereview/repository/GroupRepository.java
@@ -2,11 +2,16 @@ package com.app.gamereview.repository;
 
 import com.app.gamereview.model.Group;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.Optional;
+import java.util.List;
 
 public interface GroupRepository extends MongoRepository<Group, String> {
     Optional<Group> findByIdAndIsDeletedFalse(String id);
 
     Optional<Group> findByTitleAndIsDeletedFalse(String title);
+
+    @Query("{ 'members' : ?0, 'bannedMembers' : { $nin: [?0] }, 'isDeleted' : false }")
+    List<Group> findUserGroups(String userId);
 }
