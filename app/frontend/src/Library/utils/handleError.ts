b74fcc47facceface87f271894dc284c14f38d3c
Arda Kabadayı <kabadayi_arda@hotmail.com>
2023-11-27 01:46:00 +0300

    add global axios error handler

--- app/frontend/src/Library/utils/handleError.ts
+++ app/frontend/src/Library/utils/handleError.ts
@@ -1,4 +1,5 @@
 import { message } from "antd";
+import { NotificationUtil } from "./notification";
 
 export function handleError(error: any) {
   {
@@ -6,5 +7,12 @@ export function handleError(error: any) {
       (error as Error).message ?? `Unknown Error: ${JSON.stringify(error)}`;
     message.error(text);
     console.error(error as Error);
+  }
+}
+
+export function handleAxiosError(error: any) {
+  {
+    const errorMessage = error.response.data || `An error occurred`;
+    NotificationUtil.error(errorMessage);
   }
 }
