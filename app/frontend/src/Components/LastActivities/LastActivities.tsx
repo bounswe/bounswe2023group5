10c7673e4a4023c79598be5c907723df1be9b21a
Arda Kabadayı <kabadayi_arda@hotmail.com>
2023-11-27 21:20:16 +0300

    add last activities component

---
+++ app/frontend/src/Components/LastActivities/LastActivities.tsx
@@ -0,0 +1,46 @@
+import { getActivities } from "../../Services/activities";
+import { useQuery } from "react-query";
+import styles from "./LastActivities.module.scss";
+
+function LastActivities() {
+  const activities = useQuery(["activites"], () => getActivities());
+  console.log(activities.data);
+  return (
+    <div className={styles.container}>
+      {!activities.data ? (
+        <p>No activities</p>
+      ) : (
+        activities.data.map((activity) => {
+          return (
+            <>
+              {activity.type === "COMMENT" ? (
+                <div className={styles.activity}>
+                  <p className={styles.content}>
+                    You commented {activity.description}
+                  </p>
+                </div>
+              ) : activity.type === "REVIEW" ? (
+                <div className={styles.activity}>
+                  <p className={styles.content}>
+                    You reviewed {activity.description}
+                  </p>
+                </div>
+              ) : (
+                activity.type === "POST" && (
+                  <div className={styles.activity}>
+                    <p className={styles.content}>
+                      You posted {activity.description}
+                    </p>
+                  </div>
+                )
+              )}
+            </>
+          );
+        })
+      )}
+    </div>
+  );
+}
+
+export default LastActivities;
+//
