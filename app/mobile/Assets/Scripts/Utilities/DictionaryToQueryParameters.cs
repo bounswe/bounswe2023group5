1998f6e3815b2e04e6561e1103d7bdde896e43f6
Bilal <bilalatim27@gmail.com>
2023-11-27 14:54:05 +0300

    Implement Achievement Functionality Scripts

---
+++ app/mobile/Assets/Scripts/Utilities/DictionaryToQueryParameters.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+public static class DictionaryToQueryParameters
+{
+    public static string DictionaryToQuery(Dictionary<string,string> queryParams)
+    {
+        string queryParamsString = "";
+
+        bool isQueryParamAdded = false;
+        
+        foreach (KeyValuePair<string, string> entry in queryParams)
+        {
+            if (!isQueryParamAdded) // First query param added
+            {
+                queryParamsString = queryParamsString + "?";
+                isQueryParamAdded = true;
+            }
+            else
+            {
+                queryParamsString =  queryParamsString + "&";
+            }
+
+            queryParamsString += $"{entry.Key}={entry.Value}";
+        }
+
+        return queryParamsString;
+    }
+    
+    // Return the value of the param. If param does not exist in pars,
+    // return empty string ""
+    public static string GetValueOfParam(Dictionary<string,string> queryParams, string param)
+    {
+        if (queryParams.Count == 0) return "";
+        
+        foreach (KeyValuePair<string, string> entry in queryParams)
+        {
+            if (entry.Key == param)
+            {
+                return entry.Value;
+            }
+        }
+
+        return "";
+    }
+    
+    
+}
