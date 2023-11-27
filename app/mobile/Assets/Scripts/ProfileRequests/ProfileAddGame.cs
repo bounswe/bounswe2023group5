8bf59278073392ca1f6f7f114d6a6ac6df8de86f
Bilal <bilalatim27@gmail.com>
2023-11-27 17:38:44 +0300

    Implementation Profile Functionality Scripts

---
+++ app/mobile/Assets/Scripts/ProfileRequests/ProfileAddGame.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ProfileAddGame : MonoBehaviour
+{
+    [SerializeField] private string game;
+    private string id;
+    private Dictionary<string,string> queryParams = new Dictionary<string, string>();
+
+    private void Start()
+    {
+        queryParams.Add("id ", "1");
+        Init();
+    }
+
+    public void Init()
+    {
+        string url = AppVariables.HttpServerUrl + "/profile/add-game" +
+                     DictionaryToQueryParameters.DictionaryToQuery(queryParams);
+        var editProfileRequest = new ProfileRemoveGameRequest();
+        editProfileRequest.game = game;
+        string bodyJsonString = JsonUtility.ToJson(editProfileRequest);
+        StartCoroutine(Post(url, bodyJsonString));
+    }
+    IEnumerator Post(string url, string bodyJsonString)
+    {
+        var request = new UnityWebRequest(url, "POST");
+        byte[] bodyRaw = Encoding.UTF8.GetBytes(bodyJsonString);
+        request.uploadHandler = new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+        request.SetRequestHeader("Authorization", PersistenceManager.UserToken);
+        yield return request.SendWebRequest();
+        string response = "";
+        if (request.responseCode == 200)
+        {
+            response = request.downloadHandler.text;
+            Debug.Log("Success to add game: " + response);
+        }
+        else
+        {
+            Debug.Log("Error to add game: " + response);
+        }
+        request.downloadHandler.Dispose();
+        request.uploadHandler.Dispose();
+    }
+    
+}
