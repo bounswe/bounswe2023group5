b9f00d49ddf02fb95875b654779716f0f0c160b8
AliBasarann <ali.basaran@boun.edu.tr>
2023-11-27 18:20:31 +0300

    achievement onclick function is added and the opacity is reduced on hover

--- app/frontend/src/Components/Achievement/Achievement/Achievement.tsx
+++ app/frontend/src/Components/Achievement/Achievement/Achievement.tsx
@@ -1,15 +1,32 @@
 import React, { useState } from "react";
 import styles from "./Achievement.module.scss";
+import { useNavigate } from "react-router-dom";
 
 function Achievement({ props }: { props: any }) {
+  const navigate = useNavigate();
+
   const [isHovered, setIsHovered] = useState(false);
   const handleMouseOver = () => {
     setIsHovered(true);
   };
-
   const handleMouseLeave = () => {
     setIsHovered(false);
   };
+
+  function handleClick() {
+    if (props.onClick) {
+      props.onClick();
+      console.log("props.onClick: ");
+    } else {
+      if (!props.game) {
+        return;
+        
+      }
+      console.log("gameId: ", props.game);
+      const gameId = props.game;
+      navigate(`/game/detail/${gameId}`);
+    }
+  }
 
   return (
     <div>
@@ -17,6 +34,7 @@ function Achievement({ props }: { props: any }) {
         className={`${styles.achievement} ${isHovered ? styles.hovered : ""}`}
         onMouseOver={handleMouseOver}
         onMouseLeave={handleMouseLeave}
+        onClick={handleClick}
       >
         <img
           src={`${import.meta.env.VITE_APP_IMG_URL}${props?.icon}`}
--- app/frontend/src/Components/Achievement/SquareAchievement/SquareAchievement.tsx
+++ app/frontend/src/Components/Achievement/SquareAchievement/SquareAchievement.tsx
@@ -1,10 +1,30 @@
 import React, { useState } from "react";
 import styles from "./SquareAchievement.module.scss";
 import { Tooltip } from "antd";
+import { useNavigate } from "react-router-dom";
 
 function SquareAchievement({ props }: { props: any }) {
+
+
+  const navigate = useNavigate();
+
+  function handleClick() {
+    if (props.onClick) {
+      props.onClick();
+      console.log("props.onClick: ");
+    } else {
+      if (!props.game) {
+        return;
+        
+      }
+      console.log("gameId: ", props.game);
+      const gameId = props.game;
+      navigate(`/game/detail/${gameId}`);
+    }
+  }
+
   return (
-    <div className={styles.achievement}>
+    <div className={styles.achievement} onClick={handleClick}>
       <div className={styles.achievement_content}>
         <Tooltip
           title={props.title + ": " + props.description}
@@ -14,6 +34,7 @@ function SquareAchievement({ props }: { props: any }) {
             src={`${import.meta.env.VITE_APP_IMG_URL}${props?.icon}`}
             alt="achievement icon"
             className={styles.icon}
+            onClick={handleClick}
           ></img>
         </Tooltip>
       </div>
