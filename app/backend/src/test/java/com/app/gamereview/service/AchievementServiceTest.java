71e9e8786df17f44a49ac16048edc15d411b740e
Deniz Unal <den1z.unal@protonmail.com>
2023-12-01 01:36:50 +0300

    created initial achievement service tests

---
+++ app/backend/src/test/java/com/app/gamereview/service/AchievementServiceTest.java
@@ -0,0 +1,195 @@
+package com.app.gamereview.service;
+
+import com.app.gamereview.dto.request.achievement.CreateAchievementRequestDto;
+import com.app.gamereview.dto.request.achievement.GrantAchievementRequestDto;
+import com.app.gamereview.dto.request.achievement.UpdateAchievementRequestDto;
+import com.app.gamereview.enums.AchievementType;
+import com.app.gamereview.model.Achievement;
+import com.app.gamereview.model.Game;
+import com.app.gamereview.model.Profile;
+import com.app.gamereview.repository.AchievementRepository;
+import com.app.gamereview.repository.GameRepository;
+import com.app.gamereview.repository.ProfileRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+public class AchievementServiceTest {
+
+    @Mock
+    private AchievementRepository achievementRepository;
+
+    @Mock
+    private GameRepository gameRepository;
+
+    @Mock
+    private ProfileRepository profileRepository;
+
+    @Mock
+    private ModelMapper modelMapper;
+
+    @InjectMocks
+    private AchievementService achievementService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testCreateAchievement_GameAchievement_Success() {
+        // Arrange
+        CreateAchievementRequestDto requestDto = new CreateAchievementRequestDto();
+        requestDto.setType(AchievementType.GAME.toString());
+        requestDto.setGame("gameId");
+        Achievement achievement = new Achievement();
+        achievement.setGame("gameId");
+        achievement.setType(AchievementType.valueOf(requestDto.getType()));
+
+        when(gameRepository.findByIdAndIsDeletedFalse(anyString())).thenReturn(Optional.of(new Game()));
+        when(achievementRepository.findByTitleAndIsDeletedFalse(anyString())).thenReturn(new ArrayList<>());
+        when(modelMapper.map(any(CreateAchievementRequestDto.class), eq(Achievement.class))).thenReturn(achievement);
+        // Act
+        Achievement result = achievementService.createAchievement(requestDto);
+
+        // Assert
+        //Assertions.assertNotNull(result);
+        verify(achievementRepository, times(1)).save(any(Achievement.class));
+    }
+
+    @Test
+    void testCreateAchievement_MetaAchievement_Success() {
+        // Arrange
+        CreateAchievementRequestDto requestDto = new CreateAchievementRequestDto();
+        requestDto.setType(AchievementType.META.toString());
+        Achievement achievement = new Achievement();
+        achievement.setType(AchievementType.valueOf(requestDto.getType()));
+
+        when(achievementRepository.findByTitleAndIsDeletedFalse(anyString())).thenReturn(new ArrayList<>());
+        when(modelMapper.map(any(CreateAchievementRequestDto.class), eq(Achievement.class))).thenReturn(achievement);
+        // Act
+        Achievement result = achievementService.createAchievement(requestDto);
+
+        // Assert
+        //Assertions.assertNotNull(result);
+        verify(achievementRepository, times(1)).save(any(Achievement.class));
+    }
+
+    // Add more test cases for other scenarios (e.g., validation failures, duplicate achievements, etc.)
+
+    @Test
+    void testUpdateAchievement_Success() {
+        // Arrange
+        String achievementId = "achievementId";
+        UpdateAchievementRequestDto requestDto = new UpdateAchievementRequestDto();
+        requestDto.setTitle("New Title");
+        Achievement existingAchievement = new Achievement();
+        existingAchievement.setId(achievementId);
+        when(achievementRepository.findByIdAndIsDeletedFalse(anyString())).thenReturn(Optional.of(existingAchievement));
+        when(achievementRepository.findByTitleAndIsDeletedFalse(anyString())).thenReturn(Collections.emptyList());
+
+        // Act
+        Achievement result = achievementService.updateAchievement(achievementId, requestDto);
+
+        // Assert
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals(requestDto.getTitle(), result.getTitle());
+        verify(achievementRepository, times(1)).save(any(Achievement.class));
+    }
+
+    // Add more test cases for other scenarios (e.g., updating with duplicate title, updating non-existent achievement, etc.)
+
+    @Test
+    void testDeleteAchievementById_Success() {
+        // Arrange
+        String achievementId = "achievementId";
+        Achievement existingAchievement = new Achievement();
+        existingAchievement.setId(achievementId);
+        when(achievementRepository.findByIdAndIsDeletedFalse(anyString())).thenReturn(Optional.of(existingAchievement));
+
+        // Act
+        Achievement result = achievementService.deleteAchievement(achievementId);
+
+        // Assert
+        Assertions.assertNotNull(result);
+        Assertions.assertTrue(result.getIsDeleted());
+        verify(achievementRepository, times(1)).save(any(Achievement.class));
+    }
+
+    // Add more test cases for other scenarios (e.g., deleting non-existent achievement, deleting already deleted achievement, etc.)
+
+    @Test
+    void testDeleteAchievementByNameAndGame_Success() {
+        // Arrange
+        String achievementName = "achievementName";
+        String gameName = "gameName";
+        Achievement existingAchievement = new Achievement();
+        Game existingGame = new Game();
+
+        existingAchievement.setTitle(achievementName);
+        existingGame.setGameName(gameName);
+        existingAchievement.setGame(existingGame.getId());
+
+        when(achievementRepository.findByTitleAndIsDeletedFalse(anyString())).thenReturn(Collections.singletonList(existingAchievement));
+        when(gameRepository.findByGameNameAndIsDeletedFalse(anyString())).thenReturn(Optional.of(existingGame));
+        when(gameRepository.findByIdAndIsDeletedFalse(anyString())).thenReturn(Optional.of(existingGame));
+
+        // Act
+        Achievement result = achievementService.deleteAchievement(achievementName, gameName);
+
+        // Assert
+        Assertions.assertNotNull(result);
+        Assertions.assertTrue(result.getIsDeleted());
+        verify(achievementRepository, times(1)).save(any(Achievement.class));
+    }
+
+    // Add more test cases for other scenarios (e.g., deleting with non-existent game, deleting with non-existent achievement, etc.)
+
+    @Test
+    void testGrantAchievement_Success() {
+        // Arrange
+        String achievementId = "achievementId";
+        String userId = "userId";
+        GrantAchievementRequestDto requestDto = new GrantAchievementRequestDto();
+        requestDto.setAchievementId(achievementId);
+        requestDto.setUserId(userId);
+        when(achievementRepository.findByIdAndIsDeletedFalse(anyString())).thenReturn(Optional.of(new Achievement()));
+        when(profileRepository.findByUserIdAndIsDeletedFalse(anyString())).thenReturn(Optional.of(new Profile()));
+
+        // Act
+        List<String> result = achievementService.grantAchievement(requestDto);
+
+        // Assert
+        Assertions.assertNotNull(result);
+        verify(profileRepository, times(1)).save(any(Profile.class));
+    }
+
+    // Add more test cases for other scenarios (e.g., granting with non-existent achievement, granting with non-existent user, etc.)
+
+    @Test
+    void testGetGameAchievements_Success() {
+        // Arrange
+        String gameId = "gameId";
+        when(gameRepository.findByIdAndIsDeletedFalse(anyString())).thenReturn(Optional.of(new Game()));
+        when(achievementRepository.findByGameAndIsDeletedFalse(anyString())).thenReturn(Collections.emptyList());
+
+        // Act
+        List<Achievement> result = achievementService.getGameAchievements(gameId);
+
+        // Assert
+        Assertions.assertNotNull(result);
+        verify(achievementRepository, times(1)).findByGameAndIsDeletedFalse(anyString());
+    }
+}
