2db74cb26d36f366d251cae230890be27816cf17
Arda Kabadayı <kabadayi_arda@hotmail.com>
2023-12-11 21:17:19 +0300

    add update game to service

--- app/frontend/src/Services/games.ts
+++ app/frontend/src/Services/games.ts
@@ -36,3 +36,30 @@ export const createGame = async ({
 
   return response.data;
 };
+
+export const updateGame = async ({
+  name,
+  description,
+  releaseDate,
+  gameIcon,
+  minSystemReq,
+}: {
+  name: string;
+  description: string;
+  releaseDate: Date | null;
+  gameIcon: any;
+  minSystemReq: string;
+}) => {
+  const response = await axios.put(
+    `${import.meta.env.VITE_APP_API_URL}/game/update`,
+    {
+      gameName: name,
+      gameDescription: description,
+      releaseDate,
+      gameIcon,
+      minSystemReq: minSystemReq || "4GB RAM", // will be changed
+    }
+  );
+
+  return response.data;
+};
