d9d9d482a6b77ea0253cbe032b6b6577778e50fe
alperen-bircak <bircakalperen@gmail.com>
2023-12-11 18:54:57 +0300

    home page finished

--- app/frontend/src/Pages/HomePage/HomePage.tsx
+++ app/frontend/src/Pages/HomePage/HomePage.tsx
@@ -3,13 +3,50 @@ import styles from "./HomePage.module.scss";
 import { useAuth } from "../../Components/Hooks/useAuth";
 import { getHomePosts } from "../../Services/home";
 import ForumPost from "../../Components/Forum/ForumPost/ForumPost";
-
+import { useState } from "react";
+import { Button, Select } from "antd";
+import {
+  SortAscendingOutlined,
+  SortDescendingOutlined,
+} from "@ant-design/icons";
+const sortOptions = [
+  { label: "Creation Date", value: "CREATION_DATE" },
+  { label: "Overall Vote", value: "OVERALL_VOTE" },
+  { label: "Vote Count", value: "VOTE_COUNT" },
+];
 function HomePage() {
   const { user } = useAuth();
-  const { data, isLoading } = useQuery(["home", user?.id], getHomePosts);
+  const [sortBy, setSortBy] = useState<string>(sortOptions[0].value);
+  const [sortDirection, setSortDir] = useState<"ASCENDING" | "DESCENDING">(
+    "DESCENDING"
+  );
+  const toggleSortDir = () => {
+    setSortDir((currentSortDir) =>
+      currentSortDir === "ASCENDING" ? "DESCENDING" : "ASCENDING"
+    );
+  };
+  const { data, isLoading } = useQuery(["home", user?.id], () =>
+    getHomePosts(sortDirection, sortBy as any)
+  );
+
   return (
     <div className={styles.pageContainer}>
       <div className={styles.postsContainer}>
+        <div className={styles.filterContainer}>
+          <Button onClick={toggleSortDir}>
+            {sortDirection === "DESCENDING" ? (
+              <SortDescendingOutlined />
+            ) : (
+              <SortAscendingOutlined />
+            )}
+          </Button>
+          <Select
+            options={sortOptions}
+            value={sortBy}
+            onChange={setSortBy}
+            style={{ width: "120px" }}
+          />
+        </div>
         {data?.map((post: any) => (
           <ForumPost
             post={post}
