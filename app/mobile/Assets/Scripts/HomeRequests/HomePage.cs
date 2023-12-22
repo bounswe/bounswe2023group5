89db215f6c38f8e70eadb032c7aeef6410231139
Bilal <bilalatim27@gmail.com>
2023-12-23 01:59:01 +0300

    Change home page ui

--- app/mobile/Assets/Scripts/HomeRequests/HomePage.cs
+++ app/mobile/Assets/Scripts/HomeRequests/HomePage.cs
@@ -40,6 +40,7 @@ public class HomePage : MonoBehaviour
         else
         {
             Texture2D texture = DownloadHandlerTexture.GetContent(request);
+            targetImage.gameObject.SetActive(true);
             targetImage.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
         }
     }
