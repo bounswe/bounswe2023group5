a5cab3dcbb705b0528c9c0478779196d526c3214
said-yolcu <saidyolcu.saidyolcu@gmail.com>
2023-12-23 19:23:36 +0300

    Create "character controller"

---
+++ app/mobile/Assets/Scripts/Controllers/CharacterController.cs
@@ -0,0 +1,75 @@
+public class CharacterController
+{
+    
+}
+
+// CharacterUpdateRequest == CharacterRequest
+// Query parameters:
+// string id
+// Header parameters:
+// string Authorization
+// Response is CharacterResponse
+
+// CharacterCreateRequest == CharacterRequest
+// Header parameters:
+// string Authorization
+
+
+public class CharacterRequest
+{
+    public string name;
+    public string icon;
+    public string description;
+    public string[] games;
+    public string type;
+    public string gender;
+    public string race;
+    public string status;
+    public string occupation;
+    public string birthDate;
+    public string voiceActor;
+    public string height;
+    public string age;
+    public CustomFields customFields;
+}
+
+// CharacterGetGameCharactersRequest
+// Query parameters:
+// string gameId
+// Response is CharacterResponse[]
+
+// CharacterDeleteRequest
+// Query parameters:
+// string id
+// Header parameters:
+// string Authorization
+// Response is CharacterResponse
+
+
+public class CharacterResponse
+{
+    public string id;
+    public string createdAt;
+    public bool isDeleted;
+    public string name;
+    public string icon;
+    public string description;
+    public string[] games;
+    public string type;
+    public string gender;
+    public string race;
+    public string status;
+    public string occupation;
+    public string birthDate;
+    public string voiceActor;
+    public string height;
+    public string age;
+    public CustomFields customFields;
+}
+
+public class CustomFields
+{
+    public string additionalProp1;
+    public string additionalProp2;
+    public string additionalProp3;
+}
