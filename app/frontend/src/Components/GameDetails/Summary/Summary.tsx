9942b75622923c5c722196a9f4c52d8f078422d5
Arda Kabadayı <kabadayi_arda@hotmail.com>
2023-12-24 16:00:10 +0300

    generate the annotation id as url in game summary

--- app/frontend/src/Components/GameDetails/Summary/Summary.tsx
+++ app/frontend/src/Components/GameDetails/Summary/Summary.tsx
@@ -1,13 +1,10 @@
-import { Typography } from "antd";
+import { Tooltip, Typography } from "antd";
 import TagRenderer from "../../TagRenderer/TagRenderer";
 import styles from "./Summary.module.scss";
 import { getAchievementByGame } from "../../../Services/achievement";
 import Achievement from "../../Achievement/Achievement/Achievement";
 import { useQuery } from "react-query";
-import {
-  handleAxiosError,
-  handleError,
-} from "../../../Library/utils/handleError.ts";
+import { handleAxiosError } from "../../../Library/utils/handleError.ts";
 import { Recogito } from "@recogito/recogito-js";
 
 import "@recogito/recogito-js/dist/recogito.min.css";
@@ -19,6 +16,7 @@ import {
 import { NotificationUtil } from "../../../Library/utils/notification.ts";
 import { useState } from "react";
 import { useAuth } from "../../Hooks/useAuth.tsx";
+import { InfoCircleOutlined } from "@ant-design/icons";
 
 function Summary({ game }: { game: any }) {
   const { user } = useAuth();
@@ -27,6 +25,8 @@ function Summary({ game }: { game: any }) {
     ["achievements", game.id],
     () => getAchievementByGame({ gameId: game.id! })
   );
+
+  const pageUrl = window.location.href;
 
   const isAdmin = user?.role === "ADMIN";
 
@@ -41,7 +41,7 @@ function Summary({ game }: { game: any }) {
       r.loadAnnotations(
         `${
           import.meta.env.VITE_APP_ANNOTATION_API_URL
-        }/annotation/get-source-annotations?source=${game.id}`
+        }/annotation/get-source-annotations?source=${pageUrl}`
       )
         .then(function (annotations) {})
         .catch((error) => {
@@ -53,8 +53,8 @@ function Summary({ game }: { game: any }) {
 
       r.on("createAnnotation", async (annotation: any, _overrideId) => {
         try {
-          annotation.target = { ...annotation.target, source: game.id };
-
+          annotation.target = { ...annotation.target, source: pageUrl };
+          annotation.id = pageUrl;
           await createAnnotation(annotation);
           NotificationUtil.success("You successfully create the annotation");
         } catch (error) {
@@ -74,7 +74,8 @@ function Summary({ game }: { game: any }) {
 
       r.on("updateAnnotation", async function (annotation, _previous) {
         try {
-          annotation.target = { ...annotation.target, source: game.id };
+          annotation.target = { ...annotation.target, source: pageUrl };
+          annotation.id = pageUrl;
           await updateAnnotation(annotation);
           NotificationUtil.success("You successfully update the annotation");
         } catch (error) {
